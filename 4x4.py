ae2bcad164c6503e3508aadd4aa8dcaefe399f09
Reinjan <reinjan@vercruysse.be>
2018-04-24 13:08:37 +0200

    Basic player has entered the game

--- 4x4.py
+++ 4x4.py
@@ -16,9 +16,13 @@ ROWS = 6
 COLS = 7
 MAX_RANGE = ROWS * COLS
 TARGET = 4
-SIGNS = '_ox'
-WIN = True
-LOSE = False
+SIGNS = ' ox'
+NEUTRAL = SIGNS[0]
+WIN = 1
+LOSE = 0
+DRAW = -1
+#ELO
+K = 32
 
 def print_rijen(listje):
     for i in range(ROWS):        
@@ -47,7 +51,7 @@ def print_diagonalen(listje):
 
 def controle(rij):
     for x in range(len(rij)-3):        
-        if rij[x:x+TARGET].count(rij[x]) >= TARGET and rij[x]!=SIGNS[0]:            
+        if rij[x:x+TARGET].count(rij[x]) >= TARGET and rij[x]!=NEUTRAL:            
             return True
         
 def controle_rijen(listje):
@@ -90,6 +94,40 @@ def controle_diagonalen(listje):
         if controle(rij):
             log.debug('WIN op diagonaal-bergop {0!s} : {1!s}'.format(i+1,rij))   
             return True
+        
+def calculateElo(players,scores):
+    
+    R1 = 10 ** (scores[players[0]]/400)
+    R2 = 10 ** (scores[players[1]]/400)
+    
+    E1 = R1 / (R1 + R2)   
+    E2 = R2 / (R1 + R2)
+    
+   
+        
+    r1win = round( K*(1-E1))
+    r1lose = round( K*(0-E1))
+    r1draw = round( K*(0.5-E1))
+    
+    r2win = round( K*(1-E2))
+    r2lose = round( K*(0-E2))
+    r2draw = round( K*(0.5-E2))
+    
+    if r1win+r2lose != 0 :
+        log.warning('elo fout'+str(r1win,r2lose ))
+        
+    if r2win+r1lose != 0 :
+        log.warning('elo fout'+str(r2win,r1lose ))
+        
+    if r1draw+r2draw != 0 :
+        log.warning('elo fout'+str(r1draw,r2draw))
+    
+    elo = {
+            players[0] : {WIN : r1win,LOSE : r1lose,DRAW : r1draw},
+            players[1] : {WIN : r2win,LOSE : r2lose,DRAW : r2draw}
+            }
+    
+    return elo 
             
 class Player():   
     
@@ -100,10 +138,24 @@ class Player():
         #basic move 
         return 0
     
+class BasicPlayer():   
+    
+    def __init__(self,name):
+        self.name = name
+        
+    def makeMove(self,game_state,sign):
+        #plaats in eerste kolom die nog niet vol is
+        for x in range (COLS):
+            if game_state[(ROWS-1)*COLS + x] == NEUTRAL:
+                return x
+    
 class RandomPlayer(Player):
     def makeMove(self,game_state,sign):
         #basic move 
-        return random.randint(0,COLS-1)
+        while True:
+           x = random.randint(0,COLS-1)
+           if game_state[(ROWS-1)*COLS + x] == NEUTRAL:
+                return x
     
 class Game():
     #list
@@ -115,7 +167,7 @@ class Game():
     def __init__(self,players):
         self.players = players
         #reset bord
-        self.state = [SIGNS[0] for x in range(MAX_RANGE)]  
+        self.state = [NEUTRAL for x in range(MAX_RANGE)]  
         #geef elke speler een teken        
         self.signs[players[0]] = SIGNS[1]
         self.signs[players[1]] = SIGNS[2]    
@@ -129,7 +181,7 @@ class Game():
         for x in range(ROWS):
             try:
                 veld = col + x*COLS
-                if self.state[veld] == SIGNS[0]:
+                if self.state[veld] == NEUTRAL:
                     #illegal move
                    #log.debug(veld)
                    self.state[veld] = self.signs[player]
@@ -155,6 +207,8 @@ class Game():
                     return True,active_player,other_player
             #blijven spelen en spelrs omwisselen
             active_player,other_player = other_player,active_player
+        
+        return None,active_player,other_player
                 
 class Tornooi:
     scores = {}
@@ -163,40 +217,26 @@ class Tornooi:
         self.players = players
         
     def playgame(self,players):   
-        def addToScore(winorlose,player,opponent):       
-            #reverse winner
-            
-            self.scores[player],self.scores[opponent] = calculateElo(self.scores[player],self.scores[opponent],winorlose)
-                    
+        def addToScore(winorlose,winner,loser,elo):  
+            if winorlose==DRAW :
+                log.warning('gelijkspel')
+                self.scores[DRAW] += elo[winner][DRAW]            
+                self.scores[DRAW]  += elo[loser][DRAW]
+            else:         
+                self.scores[winner] += elo[winner][WIN]            
+                self.scores[loser]  += elo[loser][LOSE]   
         
-        def calculateElo(score1,score2,winlosedraw):
-            K = 32
-            R1 = 10 ** (score1/400)
-            R2 = 10 ** (score2/400)
-            
-            E1 = R1 / (R1 + R2)   
-            E2 = R2 / (R1 + R2)
-            
-            if winlosedraw == WIN:
-                S1 = 1      
-                S2 = 0
-            elif winlosedraw == LOSE:
-                S1 = 0
-                S2 = 1                
-            else:
-                S1 = 0.5
-                S2 = 0.5                
-            
-            return int(score1 + K*(S1-E1)),int(score2 + K*(S2-E2))
-        
+        #stakes
+        elo = calculateElo(players,self.scores)
         #play game
         winorlose,winner,loser = Game(players).play()
         if winorlose==WIN: 
             log.debug('We got a winner : '+winner.name)        
         else:
-            log.debug(loser.name+' lost by making illegal move')
-                
-        addToScore(winorlose,winner,loser)
+            log.debug(loser.name+' lost by making illegal move')               
+            
+            
+        addToScore(winorlose,winner,loser,elo)
         
     
     def run(self):
@@ -218,9 +258,12 @@ def main():
   
     #define players
     players.append(Player('player 1'))
-    players.append(RandomPlayer('player 2'))
-    players.append(Player('player 3'))
-    players.append(RandomPlayer('player 4'))
+    players.append(RandomPlayer('RandomPlayer 2.1'))
+    players.append(RandomPlayer('RandomPlayer 2.2'))
+    players.append(BasicPlayer('BasicPlayer 3.1'))
+    players.append(BasicPlayer('BasicPlayer 3.2'))
+    players.append(BasicPlayer('BasicPlayer 3.3'))
+    players.append(BasicPlayer('BasicPlayer 3.4'))
     
     tornooi = Tornooi(players)
     tornooi.run()    
